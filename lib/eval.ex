8802657f16e83c7c512c785eb6485ddc89565a54
Faraz Haider <frz.hdr.619@gmail.com>
2018-06-24 23:36:44 +0530

    Fixed if construct, handled edge case when arg is a number

--- lib/eval.ex
+++ lib/eval.ex
@@ -12,25 +12,20 @@ defmodule Eval do
   end
 
   def eval([:if, test, conseq | alt], env) do
+    alt = sanitize(alt)
     case eval(test, env) do
-      true -> eval(conseq, env)
-      false -> eval(alt, env)
+      {true,_} -> eval(conseq, env)
+      {false,_} -> eval(alt, env)
     end
   end
 
   def eval([:define, symbol | exp], env) do
-    exp =
-      if length(exp) <= 1 do
-        hd(exp)
-      end
+    exp = sanitize(exp)
       {nil, Env.put(symbol,eval(exp, env) |> elem(0), env)}
   end
 
   def eval([:set!, symbol | exp], env) do
-    exp =
-      if length(exp) <= 1 do
-        hd(exp)
-      end
+    exp = sanitize(exp)
      case Env.get(symbol,env) do
          nil -> raise "#{symbol} not defined."
          _ -> {nil, Env.put(symbol, eval(exp, env) |> elem(0),env) }
@@ -40,8 +35,10 @@ defmodule Eval do
   def eval(x, env) when is_list(x) do
     proc = eval(hd(x), env) |> elem(0)
     [_ | exp] = x
-    args = compute_args(exp, Env.new_env(env)) |> Enum.into([], fn x -> elem(x, 0) end)
-    {proc.(args), env}
+    parent_env  = env
+    child_env = Env.new_env(env)
+    args = compute_args(exp, child_env) |> Enum.into([], fn x -> elem(x, 0) end)
+    {proc.(args), parent_env}
   end
 
   def compute_args([], env) do
@@ -51,5 +48,12 @@ defmodule Eval do
   def compute_args([h | t], env) do
     {result, env} = eval(h, env)
     [{result, env} | compute_args(t, env)]
+  end
+
+  def sanitize(x) do
+    if length(x) <= 1 do
+        hd(x)
+    else x
+    end
   end
 end
